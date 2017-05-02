de9083e9181dfc427d1cdd37ac80ff83fd69f982
jelko <moin@jelkoarnds.com>
2017-05-02 20:19:09 +0200

    Initial Commit

---
+++ painfully slow typing/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  painfully slow typing
+//
+//  Created by Jelko Arnds on 02.05.2017.
+//  Copyright © 2017 Jelko Arnds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
